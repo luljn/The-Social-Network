3df6c6fac916cfed5bc74a786305c48409d58cc2
luljn <mbohlulajonathan4@gmail.com>
2024-03-30 16:25:24 +0100

    The router has been updated

--- TSN/index.php
+++ TSN/index.php
@@ -3,4 +3,16 @@
 require_once("src/controllers/home/home.php");
 use TSN\src\controllers\home\Home as Home;
 
-(new Home)->getHomePage();
+require_once("src/controllers/error404/error404.php");
+ use TSN\src\controllers\error404\Error as Error;
+
+
+try {
+    
+    (new Home)->getHomePage();
+}
+
+catch(Exception $e) {
+
+    // (new Error)->getError404Page();    
+}
