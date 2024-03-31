04641378914b0479404cf1f9ef51024e817003f0
luljn <mbohlulajonathan4@gmail.com>
2024-03-31 17:27:48 +0200

    The login page has been setted as the default site page

--- TSN/index.php
+++ TSN/index.php
@@ -6,10 +6,14 @@ use TSN\src\controllers\home\Home as Home;
 require_once("src/controllers/error404/error404.php");
  use TSN\src\controllers\error404\Error as Error;
 
+ require_once("src/controllers/login/login.php");
+ use TSN\src\controllers\login\Login as Login;
+
 
 try {
     
-    (new Home)->getHomePage();
+    // (new Home)->getHomePage();
+    (new Login)->getLoginPage();  // The default page of the site.
 }
 
 catch(Exception $e) {
