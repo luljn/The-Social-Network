883f3c2529664070cb2b08013fd660dd50a059d7
luljn <mbohlulajonathan4@gmail.com>
2024-03-31 17:12:50 +0200

    Login page has been added

---
+++ TSN/src/views/login.php
@@ -0,0 +1,48 @@
+<?php $title = "TSN - Connexion"; ?>
+ 
+<?php 
+    ob_start();
+    require('header.php');
+    $header = ob_get_clean(); 
+?>
+
+<?php ob_start(); ?>
+
+    <div class="container-fluid">
+        <div class="row vh-100">
+            <div class="col-12 col-sm-6 col-md-8 p-0">
+                <img class="img-fluid h-100" src="https://picsum.photos/1920/1080?random=24" alt="Image page de connexion">
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-center px-5 px-sm-5 py-5 py-sm-5">
+                <form action="" class="row gy-4 pb-4 border border-3 rounded-3">
+                    <div class="col-12">
+                              <label for="email" class="form-label">Adresse Mail</label>
+                              <input type="text" class="form-control" name="email" id="email">
+                    </div>
+                    <div class="col-12">
+                        <label for="mdp" class="form-label">Mot de passe</label>
+                        <input type="password" class="form-control" name="mdp" id="mdp">
+                    </div>
+                    <div class="col-12">
+                      <button type="submit" class="btn btn-primary w-100">Se Connecter</button>
+                    </div>
+                    <p>
+                        Pas encore de compte ?
+                        <a href="inscription.html">
+                            Inscrivez-vous Ici
+                        </a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php 
+    ob_start();
+    require('footer.php');
+    $footer = ob_get_clean(); 
+?>
+
+<?php require('layout.php') ?>
