22a6db0f822eee4ea37c077eed0aa7fa1b6b3713
luljn <mbohlulajonathan4@gmail.com>
2024-04-01 17:09:41 +0200

    Function connectUser added to Login class (in the package models)

--- TSN/src/models/login.php
+++ TSN/src/models/login.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 namespace TSN\src\models\login;
 
 use TSN\src\models\lib\DatabaseConnection;
@@ -12,10 +11,32 @@ class Login {
     private User $user;
     private DatabaseConnection $databaseConnection;
 
-    public function connectUser(){
+    public function connectUser($email, $password){
         
         $this->databaseConnection = new DatabaseConnection;
 
-        
+        $statement_1 = "SELECT COUNT(email) AS email from utilisateurs WHERE email = '{$email}';";
+        $statement_2 = "SELECT mdp AS password from utilisateurs WHERE email = '{$email}';";
+
+        $query_1 = $this->databaseConnection->getConnection()->prepare($statement_1);
+        $query_2 = $this->databaseConnection->getConnection()->prepare($statement_2);
+
+        $query_1->execute();
+        $query_2->execute();
+
+        $result_1 = $query_1->fetch();
+        $result_2 = $query_2->fetch();
+
+        $query_1->closeCursor();
+        $query_2->closeCursor();
+
+        if($result_1['email'] === 1 and password_verify($password, $result_2['mdp'])){
+
+            header("location: http://localhost:4000/index.php?action=home");
+        }
+
+        else{
+
+        }
     }
 }
