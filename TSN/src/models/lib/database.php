1a4492d0a4c6986782e5f7f18e93a697bfc5a55a
luljn <mbohlulajonathan4@gmail.com>
2024-04-01 13:52:26 +0200

    DatabaseConnection class added

---
+++ TSN/src/models/lib/database.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace TSN\src\models\lib;
+
+
+class DatabaseConnection {
+
+    public ?\PDO $database = null;
+
+    public function getConnection(): \PDO {
+
+        if($this->database === null){
+
+            $this->database = new \PDO("mysql:host=localhost;dbname=mbeck--selatchom--database;charset=utf8", "root", "");
+        }
+
+        return $this->database;
+    }
+}
