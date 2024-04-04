8e55079332ffafeac83c33ef91f2b357ec05e79e
luljn <mbohlulajonathan4@gmail.com>
2024-04-04 23:07:48 +0200

    Signup class added(in the models package)

---
+++ TSN/src/models/signup.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace TSN\src\models\signup;
+
+require_once("lib/database.php");
+use TSN\src\models\lib\DatabaseConnection;
+
+require_once("user.php");
+use TSN\src\models\user\User;
+
+
+class Signup {
+
+    private DatabaseConnection $databaseConnection;
+}
