d777accbcfa41d57883c311932ff158d39302b83
luljn <mbohlulajonathan4@gmail.com>
2024-04-09 22:33:19 +0200

    User followings managment modal added to the user profile page

--- TSN/src/views/profile.php
+++ TSN/src/views/profile.php
@@ -44,7 +44,7 @@
                                                 <button type="button" class="btn btn-primary col-12" data-bs-toggle="modal" data-bs-target="#userPasswordModal">Modifier mon mot de passe</button>
                                                 <button type="button" class="btn btn-primary col-12" data-bs-toggle="modal" data-bs-target="#userPhotoModal">Modifier ma photo de profil</button>
                                                 <button type="button" class="btn btn-primary col-12" data-bs-toggle="modal" data-bs-target="#userFollowersModal">Gérer mes followers</button>
-                                                <button type="button" class="btn btn-primary col-12">Gérer mes followings</button>
+                                                <button type="button" class="btn btn-primary col-12" data-bs-toggle="modal" data-bs-target="#userFollowingsModal">Gérer mes followings</button>
                                             </div>
                                         </div>
                                     </div>
@@ -151,6 +151,25 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h1 class="modal-title fs-5" id="staticBackdropLabel">Mes followers</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                                    <!-- <button type="button" class="btn btn-primary">Enregistrer</button> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- User followings managment modal -->
+                    <div class="modal fade" id="userFollowingsModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Mes followings</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
