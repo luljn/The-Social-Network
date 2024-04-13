ce95b297ff71fb4c543d77bf30510cb1b8f2a5ee
luljn <mbohlulajonathan4@gmail.com>
2024-04-13 23:14:05 +0200

    Getters and setters added to the class Post

--- TSN/src/models/post.php
+++ TSN/src/models/post.php
@@ -22,4 +22,16 @@ class Post {
         $this->idUser = $_idUser;
         $this->image = $_image;
     }
+
+    public function getID(){ return $this->id; }
+    public function getContent(){ return $this->content; }
+    public function getCreationDate(){ return $this->creationDate; }
+    public function getIdUser(){ return $this->idUser; }
+    public function getImage(){ return $this->image; }
+
+    public function setID(int $_id){ $this->id = $_id; }
+    public function setContent(string $_content){ $this->content = $_content; }
+    public function setCreationDate(string $_creationDate){ $this->creationDate = $_creationDate; }
+    public function setIdUser(int $_idUser){ $this->idUser = $_idUser; }
+    public function setImage(string $_image){ $this->image = $_image; }
 }
