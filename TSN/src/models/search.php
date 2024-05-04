ebed42629dd37441bb5ce885bd4319aaf413fbb8
luljn <mbohlulajonathan4@gmail.com>
2024-05-04 22:44:47 +0200

    Seacrh feature completely implemented (the research is made among the users and the posts)

--- TSN/src/models/search.php
+++ TSN/src/models/search.php
@@ -9,24 +9,34 @@ use TSN\src\models\lib\DatabaseConnection;
 require_once("user.php");
 use TSN\src\models\user\User as User;
 
+require_once("post.php");
+use TSN\src\models\post\Post as Post;
+
+require_once("./src/controllers/comment/comment.php");
+use TSN\src\controllers\comment\Comment as CommentController;
+
 
 class Search {
 
     private DatabaseConnection $databaseConnection;  
+    private CommentController $commentController;
     
     public function makeSearch($research){
 
         $this->databaseConnection = new DatabaseConnection;
+        $this->commentController = new CommentController;
 
-        $findingWord = str_replace(' ', '%', $research);
+        $findingWord1 = str_replace(' ', '%', $research);
+        $findingWord2 = str_replace('\'', '', $findingWord1);
+        $findingWord = str_replace('"', '', $findingWord2);
 
-        $statement = "SELECT * FROM utilisateur WHERE nom LIKE \"%{$findingWord}%\" or prenom LIKE \"%{$findingWord}%\";";
+        $statement = "SELECT * FROM utilisateur WHERE nom LIKE \"%{$findingWord}%\" or prenom LIKE \"%{$findingWord}%\" or description LIKE \"%{$findingWord}%\";";
         $query = $this->databaseConnection->getConnection()->prepare($statement);
         $query->execute();
         $result = $query->fetchAll();
         $query->closeCursor();
 
-        $statement_1 = "SELECT * FROM post WHERE contenu LIKE '%\"{$findingWord}\"%';";
+        $statement_1 = "SELECT * FROM post WHERE contenu LIKE \"%{$findingWord}%\";";
         $query_1 = $this->databaseConnection->getConnection()->prepare($statement_1);
         $query_1->execute();
         $result_1 = $query_1->fetchAll();
@@ -68,9 +78,77 @@ class Search {
 
         if($result_1 != NULL){
 
+            $Users = []; // The authors of each posts.
+
             for($i = 0; $i < count($result_1); $i++){
 
+                $idUser = $result_1[$i]['id_utilisateur'];
+                $statement_2 = "SELECT * from utilisateur WHERE id = \"{$idUser}\";";
+                $query_2 = $this->databaseConnection->getConnection()->prepare($statement_2);
+                $query_2->execute();
+                $result_2 = $query_2->fetch();
+                $query_2->closeCursor();
+
+                if($result_2['profile_photo'] == NULL){
+
+                    $user = new User($result_2['id'], $result_2['email'], $result_2['mdp'], $result_2['nom'], $result_2['prenom'],
+                                    date("d-m-Y", strtotime($result_2['date_de_naissance'])), $result_2['adresse'], $result_2['admin'], '', $result_2['description']);
+                }
+        
+                else{
+        
+                    $user = new User($result_2['id'], $result_2['email'], $result_2['mdp'], $result_2['nom'], $result_2['prenom'],
+                                    date("d-m-Y", strtotime($result_2['date_de_naissance'])), $result_2['adresse'], $result_2['admin'], $result_2['profile_photo'], $result_2['description']);
+                }
+    
+                if(!in_array($user, $Users)){
+    
+                    $Users[] = $user;   // We add the user to the list.
+                }
+            }
+
+            for ($i = 0; $i < count($result_1); $i++){     // To associate a post to his author.
+
+                foreach($Users as $ramdomUser){
+    
+                    if($result_1[$i]['id_utilisateur'] === $ramdomUser->getID()){
+    
+                        // List of comments of each post.
+                        $postComments = $this->commentController->getPostComments($result_1[$i]['id']);
+                        //
+    
+                        // To retrieve the number of likes of the post.
+                        $idPost = $result_1[$i]['id'];
+                        $statement_3 = "CALL CountLikesOfPost({$idPost});";
+                        $query_3 = $this->databaseConnection->getConnection()->prepare($statement_3);
+                        $query_3->execute();
+                        $result_3 = $query_3->fetch();
+                        $query_3->closeCursor();
+    
+                        if($result_1[$i]['image'] == NULL){
+    
+                            $userPost = new Post($result_1[$i]['id'], $result_1[$i]['contenu'], $result_1[$i]['date_creation'], $ramdomUser, '', $postComments, $result_3['NumberOfLikes']);
+                        }
+        
+                        else{
+        
+                            $userPost = new Post($result_1[$i]['id'], $result_1[$i]['contenu'], $result_1[$i]['date_creation'], $ramdomUser, $result_1[$i]['image'], $postComments, $result_3['NumberOfLikes']);
+                        }
+                        
+                        if(!in_array($userPost, $ResultPosts)){
+    
+                            $ResultPosts[] = $userPost;
+                        }
+                    }
+                }
             }
         }
+
+        else{
+
+            $ResultPosts = $result_1;
+        }
+
+        $_SESSION['resultPosts'] = $ResultPosts;
     }
 }
--- TSN/src/views/search.php
+++ TSN/src/views/search.php
@@ -15,6 +15,7 @@
     $startingUrl = (new ModelConfig)->getStartingUrl();
 
     $resultUsers = $_SESSION['resultUsers']; // All the users retrieved by the search.
+    $resultPosts = $_SESSION['resultPosts']; // All the posts retrieved by the search.
 ?>
 
     <section>
@@ -22,41 +23,81 @@
             <div class="row justify-content-center mt-5">
                     
                 
-                <?php if(!empty($resultUsers)){ 
-                        $number = 1;
-                ?>
+                <?php if(!empty($resultUsers) || !empty($resultPosts)){ ?>
                     <h5 class="fs-3 fw-bold text-center text-secondary mt-5 mb-1">Résultats</h5>
-                    <table class="table table-primary mt-5">
-                        <caption align="top" class="text-center text-secondary fs-3 fw-bold mb-3">Utilisateurs</caption>
-                        <thead>
-                            <tr>
-                                <th scope="col" class="fs-4">#</th>
-                                <th scope="col" class="fs-4">Nom</th>
-                                <th scope="col" class="fs-4">Prénom</th>
-                                <th scope="col" class="fs-4">Description</th>
-                                <th scope="col" class="fs-4">#</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($resultUsers as $user){ ?>
+                    <?php if(!empty($resultUsers)){ 
+                            $number = 1;    
+                    ?>
+                        <table class="table table-primary mt-5">
+                            <caption align="top" class="text-center text-secondary fs-3 fw-bold mb-3">Utilisateurs</caption>
+                            <thead>
                                 <tr>
-                                    <th scope="row"><?= $number ?></th>
-                                    <td><?= $user->getName() ?></td>
-                                    <td><?= $user->getSurname() ?></td>
-                                    <td><?= $user->getDescription() ?></td>
-                                    <td>
-                                        <a href="index.php?action=myAccount&userId=<?= urlencode($user->getID()) ?>">
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                                voir le compte
-                                            </button>
-                                        </a>
-                                    </td>
+                                    <th scope="col" class="fs-4">#</th>
+                                    <th scope="col" class="fs-4">Nom</th>
+                                    <th scope="col" class="fs-4">Prénom</th>
+                                    <th scope="col" class="fs-4">Description</th>
+                                    <th scope="col" class="fs-4">#</th>
                                 </tr>
-                            <?php $number++; 
-                                  } 
-                            ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach($resultUsers as $user){ ?>
+                                    
+                                    <tr>
+                                        <th scope="row"><?= $number ?></th>
+                                        <td><?= $user->getName() ?></td>
+                                        <td><?= $user->getSurname() ?></td>
+                                        <td><?= $user->getDescription() ?></td>
+                                        <td>
+                                            <a href="index.php?action=myAccount&userId=<?= urlencode($user->getID()) ?>">
+                                                <button type="button" class="btn btn-primary">
+                                                    voir le compte
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php $number++; 
+                                    } 
+                                ?>
+                            </tbody>
+                        </table>
+                    <?php } ?>
+
+                    <?php if(!empty($resultPosts)){ 
+                            $numberP = 1;    
+                    ?>
+                        <table class="table table-primary mt-5">
+                            <caption align="top" class="text-center text-secondary fs-3 fw-bold mb-3">Posts</caption>
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="fs-4">#</th>
+                                    <th scope="col" class="fs-4">Contenu</th>
+                                    <th scope="col" class="fs-4">Auteur</th>
+                                    <th scope="col" class="fs-4">Nombre de likes</th>
+                                    <th scope="col" class="fs-4">#</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($resultPosts as $post){ ?>
+                                    <tr>
+                                        <th scope="row"><?= $numberP ?></th>
+                                        <td><?= $post->getContent() ?></td>
+                                        <td><?= $post->getUser()->getSurname() . " " .  $post->getUser()->getName() ?></td>
+                                        <td><?= $post->getLikes() ?></td>
+                                        <td>
+                                            <a href="index.php?action=myAccount&userId=<?= urlencode($post->getUser()->getID()) ?>">
+                                                <button type="button" class="btn btn-primary">
+                                                    voir le compte de l'auteur
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php $numberP++; 
+                                    } 
+                                ?>
+                            </tbody>
+                        </table>
+                    <?php } ?>
+
 
                 <?php } else { ?>
                     <h5 class="fs-3 fw-bold text-center text-secondary my-5">Aucun résultat...</h5>
